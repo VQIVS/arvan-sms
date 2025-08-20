e29e61faf2594841815f8bf6e7c9e9b65d752f4f
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2025-08-20 14:15:18 +0330

    ref: change naming

--- pkg/constants/rabbit.go
+++ pkg/constants/rabbit.go
@@ -3,4 +3,6 @@ package constants
 const (
 	QueueSMSUpdate         = "finance.sms.update"
 	QueueUserBalanceUpdate = "sms.user.balance.update"
+	TopicExchange          = "topic"
+	maxAttempts            = 3
 )
